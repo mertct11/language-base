589fac1d0669e6c0e73a156eb5017b5c77b05e96
mertcan <mertct111@gmail.com>
2021-11-22 12:33:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div
+          onClick={() => {
+            i18n.changeLanguage("tr");
+          }}
+        >
+          tr
+        </div>
+        <div
+          onClick={() => {
+            i18n.changeLanguage("en");
+          }}
+        >
+          en
+        </div>
+        <h1>{t("title")}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
